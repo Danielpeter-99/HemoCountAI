6e88b0b8bafb24a37d2ad103722b3dce81cb4d11
Danielpeter-99 <apete053@fiu.edu>
2024-04-22 22:15:30 -0300

    Update BloodChart component to include legend and capitalize labels

--- frontend/components/BloodChart.tsx
+++ frontend/components/BloodChart.tsx
@@ -1,33 +1,38 @@
 import { Doughnut } from 'react-chartjs-2';
-import { Chart, ArcElement, CategoryScale, Title, Tooltip } from 'chart.js';
+import { Chart, ArcElement, CategoryScale, Title, Tooltip, Legend } from 'chart.js';
 
-Chart.register(ArcElement, CategoryScale, Title, Tooltip);
-
+Chart.register(ArcElement, CategoryScale, Title, Tooltip, Legend);
 
 function BloodChart({ bloodData }: { bloodData: Record<string, number> }) {
-    // const bloodData = {
-    //     'blood': 84,
-    //     'eosinophils': 2,
-    //     'red': 82,
-    // }
+    
+    const capitalizeFirstLetter = (str: string) => {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    };
 
     const data = {
-        labels: Object.keys(bloodData),
+        labels: Object.keys(bloodData).map((key) => capitalizeFirstLetter(key)),
         datasets: [{
             data: Object.values(bloodData),
             backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
             borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
-            borderWibloodDatah: 1,
+            borderWidth: 1,
         }],
     };
 
     return (
-        <div className="flex flex-col items-center justify-center p-24">
         <Doughnut
+            className="flex flex-col items-center justify-center p-24"
             data={data}
-            // ...props
+            options={{
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'top',
+                    },
+                },
+                aspectRatio: 1, // Adjust the value to make the chart smaller or larger
+            }}
         />
-        </div>
     )
 }
 
